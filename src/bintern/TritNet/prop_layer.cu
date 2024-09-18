2e2510af5d71b4377277d11135fd07da2eb2312d
ryanjakeherian <ryancherian@hotmail.com>
2024-09-18 01:40:10 +0100

    added comment

--- src/bintern/TritNet/prop_layer.cu
+++ src/bintern/TritNet/prop_layer.cu
@@ -20,7 +20,7 @@ void TritNet<T>::propagate_layer(int i, T *&d_A, T *&d_W, T *&d_O){ //pytorch an
     propagate<T><<<gridDim,blockDim>>>(d_A, d_W, d_O);
 
     A_list[i+1] = new T[n*layers[i+1]+1](); //this auto-initialises all to zero. FOR SOME REASON, IF I DONT INITIALISE THE LAST ELEMENT (be that thru either initialising the whole array, or just the last element), THE FIRST TIME DOING THE FORWARD PASS FAILS TO TRANSFER THE LAST ELEMENT.
-    // A_list[i+1][n*layers[i+1]] = 0;
+    // A_list[i+1][n*layers[i+1]] = 0;  //for really large layers, it might be faster just to init the last element. this bug is super annoying.
     if (cudaSuccess != cudaMemcpy(A_list[i+1], d_O, n*A_bytesizes[i+1]+1, cudaMemcpyDeviceToHost) ) {throw std::runtime_error("CUDA memcpy failed");};
 
     return;
